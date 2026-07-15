80ef3a17740a15f3dd105a812df1636d86f370c5
Clay Foye <clay.foye@gmail.com>
2026-07-15 14:36:39 +0200

    read actors cleanup

--- src-lua/read_actors.lua
+++
@@ -1,8 +0,0 @@
-local csv = require "csv"
-local f = csv.open("../Data/competitions.csv", {header=true})
-
-for fields in f:lines() do
-    for i, v in pairs(fields) do print(i,v) end
-    print("\n")
-end
-
