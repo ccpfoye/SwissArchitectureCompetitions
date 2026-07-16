8e046cb19db235f6036fdd0f3f995c42ccadade0
Clay Foye <clay.foye@gmail.com>
2026-07-16 15:56:59 +0200

    list nodes

--- src-lua/swiss_comps_graph.lua
+++ src-lua/swiss_comps_graph.lua
@@ -13,3 +13,5 @@ comp_graph.edge_list_key = "jugement_pairs"
 comp_graph:compute_degree_centrality()
 comp_graph:compute_betweenness_centrality()
 
+comp_graph:list_nodes_by_degree("canonical_name")
+
